93c473431e6ef73ab0c5e21989d9160c57f9b3f4
ChoiHoSeong <chs3112a@gmail.com>
2026-07-27 23:04:32 +0900

    feat: add global exception handler

---
+++ src/main/java/com/reservehub/resource/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.reservehub.resource.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.reservehub.resource.application.ResourceNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Void> handleResourceNotFound(
+            ResourceNotFoundException exception
+    ) {
+        return ResponseEntity.notFound().build();
+    }
+}
